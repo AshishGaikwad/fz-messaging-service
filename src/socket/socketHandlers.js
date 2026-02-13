a49b018aa85e0c3d66f9ef4a0d448d8c909351ff
Ashish Gaikwad <ashishgaikwad@gmail.com>
2026-02-13 21:23:26 +0530

    enhanced socket

--- src/socket/socketHandlers.js
+++ src/socket/socketHandlers.js
@@ -63,7 +63,7 @@ function registerSocketHandlers(io) {
           const tokens = userService.getUserExpoTokens(toUserId);
           if (tokens.length > 0) {
             await expoService.sendPush(tokens, 'New Message', message.text, {
-              senderId: message.sender,
+              senderId: toUserId,
             });
             logger.info('Expo push sent', { toUserId, tokens: tokens.length });
           }
